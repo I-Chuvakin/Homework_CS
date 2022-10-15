a8d206913d98bd6eead0fba6994155cee336daea
ILYA <ilyaxch@rambler.ru>
2022-10-15 23:44:05 +0300

    добавлено ДЗ №2

---
+++ Task13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13. Напишите программу, котор. выводит третью цифру заданного числа или сообщает,
+// что третьей цифры нет.
+
+Console.Clear();
+Console.Write("Введите трёхзначное число, или более: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int a = -404;
+if (number < 100)
+{
+      Console.WriteLine("Третьей цифры нет!");
+}
+else 
+while (number >= 100)
+{
+    a = number % 10;
+    number = number / 10;
+}
+Console.WriteLine($"Третья цифра числа: {a}");
---
+++ Task15/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 15. Напишите программу, котор. приннимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным. Прим. 6-> да, 7 -> да, 1 -> нет. 
+Console.Clear();
+Console.Write("Введите номер дня недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if ((num < 1) | (num > 7))
+{
+    Console.WriteLine("некорректный номер");
+}
+else
+{
+    if ((num == 6) | (num == 7))
+    {
+        Console.WriteLine("да");
+    }
+    else
+    {
+        Console.WriteLine("нет");
+    }
+}
